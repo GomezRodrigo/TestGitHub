8acf349752340ba97d27e63fbb7defdaed9dca52
GomezRodrigo <gomezrodrigo93@hotmail.com>
2015-07-29 21:24:22 -0300

    cambio por rodrigo gomez

--- ConsolaTestGitHub/ConsolaTestGitHub/Program.cs
+++ ConsolaTestGitHub/ConsolaTestGitHub/Program.cs
@@ -18,6 +18,8 @@ namespace ConsolaTestGitHub
             Console.ReadLine();
             Console.WriteLine("Probando proyecto en comun. Esto es clonar un repositorio online");
             Console.ReadLine();
+            Console.WriteLine("HOLA PROFE LE PUEDO ROMPER EL PROYECTO?")
+                Console.ReadLine();
         }
     }
 }
